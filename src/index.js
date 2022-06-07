ae390a689dbec744c3c035a3a6496e355710d438
Denis <sined.petrov@yandex.ru>
2022-06-08 01:21:14 +0300

    lesson 31

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
+
+const App = () => {
+
+  const todoData = [
+    { label: 'Drink Cofee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+  return (
+    <div>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList todos={todoData} />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <h1> Todo List</h1>
-      <input placeholder='search' />
-      <ul>
-        <li>Learn React</li>
-        <li>Build Awesome App</li>
-      </ul>
-      </div>
+   <App />
   </React.StrictMode>
 );
 
